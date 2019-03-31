407405811ebe81918d2a0e7d031becdf30e3f250
Daniel <c-daniel.pak@charter.com>
2019-03-31 17:23:26 -0600

    Refactored index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 //import a library to help us build a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 //create a component - The "App" Component
-const App = () => {
-  return (
-    <Text>Some Text</Text>
-  );
-};
+const App = () => <Text>Some Text</Text>;
 
 //render it to a device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
