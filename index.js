f4282398a13d29e7dfdbc39850c739f7e44a3d75
Daniel <c-daniel.pak@charter.com>
2019-03-31 17:14:30 -0600

    Removing unneeded comments

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// removing all code to establish a deep understanding of the index.js file
-
 //import a library to help us build a component
 import React from 'react';
 import ReactNative from 'react-native';
 
-//create a component
+//create a component - The "App" Component
 const App = () => {
   return (
-    <Text>Some Text</Text> //looks a lot like html tags but it's jsx
+    <Text>Some Text</Text>
   );
 };
 
