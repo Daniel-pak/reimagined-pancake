8879e329d22baeb5596643e4b29565da3a5a7fd2
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-07 14:10:06 -0600

    Adding in Album list file for list view

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //import a library to help us build a component
 import React from 'react'; //If installing package through npm, you can use the short hand. ex: react, react-native
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header'; //If creating custom components, need to reference the relative path.
+import AlbumList from './src/components/AlbumList';
 
 //create a component - The "App" Component
 const App = () => (
-  <Header headerText={'Albums'} /> //passing in prop value to component
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 //render it to a device
