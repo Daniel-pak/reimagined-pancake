c218ee7f53580babb55580f6c2e81b7937743770
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-07 07:44:05 -0600

    Adding in more complete styling to header

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,16 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
+    paddingTop: 35,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 25
