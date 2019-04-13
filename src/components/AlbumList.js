890831e188757aee2665b1bebb8d613764a5f08a
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-13 10:14:20 -0600

    Adding in react album list error fix by adding in key property

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,15 +5,27 @@ import axios from 'axios';
 //functional component -> must return back some JSX
 //class based component -> more knowledge about their overall status (life cycle methods)
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>
+    );
   }
 
   render() {
+    console.log(this.state.albums);
+
+    console.log(this.renderAlbums);
+
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
