d737d30a27cbaef7302d04a9fb1e97ebb5ec43c7
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-22 20:11:08 -0600

    Adding in new scrollable feature. Content cards list is now scrollable and smooth

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import AlbumList from './src/components/AlbumList';
 
 //create a component - The "App" Component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
