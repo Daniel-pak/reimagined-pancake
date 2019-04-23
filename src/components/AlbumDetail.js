eac6168fadb2b1f70db168e1bcde05c29b42ab05
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-22 22:24:13 -0600

    Adding in button styling - no functionality

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 //can be a functional method because we don't need a life cycle method nor state
 
@@ -13,15 +14,20 @@ const AlbumDetail = ({ album }) => {
     <Card>
       <CardSection>
         <View style={thumbnailContainerStyle}>
-          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }}/>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle}>
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
+      
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   )
